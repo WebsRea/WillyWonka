4e4944febc3c2c171553132c21561b90d7dcd702
Miguel Tello Sanz <10000714.joan23@fje.edu>
2017-05-05 19:53:15 +0200

    Afegir Activitat actualizado
    Falta conseguir obtener el cla_id del select en la tabla usuario (según
    la sesión del usuario) para insertarlo en la tbl_activitats

--- WillyWonka_web/php/procs/afegirActivitat.proc.php
+++ WillyWonka_web/php/procs/afegirActivitat.proc.php
@@ -1,16 +1,27 @@
 <?php session_start();
 extract($_REQUEST);
 
+//SELECT * FROM tbl_activitats INNER JOIN tbl_usuari ON tbl_activitats. cla_id WHERE tbl_usuari. cla_id
 
+//SELECT tbl_activitats.cla_id FROM tbl_activitats INNER JOIN tbl_usuari ON tbl_activitats.cla_id = tbl_usuari.cla_id
+
+//SELECT tbl_usuari.cla_id FROM tbl_usuari INNER JOIN tbl_activitats ON tbl_usuari.cla_id = tbl_activitats.cla_id
 
 $usu_id = $_SESSION['usu_id'];
+echo "$usu_id";
 include '../conexio.php';
-$sql = "INSERT INTO `tbl_activitats` (`act_id`, `act_titol`, `act_data`, `act_text`, `cla_id`) VALUES (NULL, '$act_titol', $act_data', '$act_text', NULL);";
+$cla_id = "";
+$query = "SELECT `cla_id` FROM `tbl_usuari` WHERE usu_id = $usu_id";
+// echo "$query";
+mysqli_query($conexio, $query);
+echo "mysqli_query($conexio, $query)";
+//$cla_id = mysqli_query($conexio, $query);
+$sql = "INSERT INTO `tbl_activitats` (`act_titol`, `act_data`, `act_text`, `cla_id`) VALUES ('$act_titol', '$act_data', '$act_text', '$cla_id');";
  echo "$sql";
 mysqli_query($conexio, $sql);
 
 
-header('Location:../mestre.php');
+// header('Location:../mestre.php');
 
 
 
