665463b93a021403ba0cf6efde12359501bbb394
Miguel Tello Sanz <10000714.joan23@fje.edu>
2017-05-05 16:15:11 +0200

    afegirEsdeveniment arreglado

--- WillyWonka_web/php/procs/afegirEsdeveniment.proc.php
+++ WillyWonka_web/php/procs/afegirEsdeveniment.proc.php
@@ -4,7 +4,7 @@ extract($_REQUEST);
 
 $usu_id = $_SESSION['usu_id'];
 include '../conexio.php';
-$sql = "INSERT INTO `tbl_esdeveniments` (`esd_id`, `esd_titol`, `esd_data`, `esd_text`,`usu_id`) VALUES (NULL, '$esd_titol', $esd_data', '$esd_text', '$usu_id');";
+$sql = "INSERT INTO `tbl_esdeveniments` (`esd_titol`, `esd_data`, `esd_text`,`usu_id`) VALUES ('$esd_titol', '$esd_data', '$esd_text', '$usu_id');";
  echo "$sql";
 mysqli_query($conexio, $sql);
 
