51e1df75027578523b047bb5875e1e68b433850b
Miguel Tello Sanz <10000714.joan23@fje.edu>
2017-05-10 20:42:02 +0200

    FormContactFamilia Acabado

--- WillyWonka_web/php/mostrarEsdeveniment.php
+++ WillyWonka_web/php/mostrarEsdeveniment.php
@@ -6,30 +6,25 @@ $usu_id = $_SESSION['usu_id'];
 include '/conexio.php';
 	?>
 <!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<div>
-<?php
-$sql = "SELECT * FROM tbl_esdeveniments WHERE `esd_data` >= DATE_SUB(CURDATE(),INTERVAL 0 DAY);";
-	$esdeveniments=mysqli_query($conexio, $sql);
-    if (mysqli_num_rows($esdeveniments) != 0){
-      while ($esdeveniment = mysqli_fetch_array($esdeveniments)) {
-		$titol = $esdeveniment['esd_titol'];
-		$descripcio = $esdeveniment['esd_text'];
-	
-		?>
-<table style="border: 1px;">
-	<tr>
-		<?php 
-			echo "
-			<th>$titol</th></tr>
-			<tr><th>$descripcio</th></tr>
-			";
-		?>
-</tr>
-</table>
-</div>
-</body>
+
+<html> 
+<body> 
+  
+<?php 
+
+$sql="SELECT esd_titol, esd_text FROM tbl_esdeveniments ORDER BY esd_data DESC"; 
+$results=mysqli_query($conexio,$sql);
+if ($row = mysqli_fetch_array($results)){ 
+   echo "<table border = '1'> \n"; 
+   echo "<tr><td>Titol</td><td>Descripció</td></tr> \n"; 
+   do { 
+      echo "<tr><td>".$row["esd_titol"]."</td><td>".$row["esd_text"]."</td></tr> \n"; 
+   } while ($row = mysqli_fetch_array($results)); 
+   echo "</table> \n"; 
+} else { 
+echo "¡ No se ha encontrado ningún registro !"; 
+} 
+?> 
+  
+</body> 
 </html>
