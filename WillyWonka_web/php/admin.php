0b912e18c454bdcc2e221f3f1eca97778ced28f4
10000585 <10000585.joan23@fje.edu>
2017-05-16 14:42:16 +0200

    Admin.php
    Lo comentamos porque se substituye por el index_admin

--- WillyWonka_web/php/admin.php
+++ WillyWonka_web/php/admin.php
@@ -1,21 +1,21 @@
-<?php session_start();
+<?php 
 
-include('conexio.php');
+// include('conexio.php');
 
-$sql = "SELECT * FROM tbl_usuari WHERE usu_id = " . $_SESSION['usu_id'] . "";
-$resultat=mysqli_query($conexio, $sql);
-				// echo "foisndfuignfdiuog";
-	if (mysqli_num_rows($resultat) != 0 ) {
-		while ($usuari = mysqli_fetch_array($resultat)) {
-			$_SESSION['usu_id'] = $usuari['usu_id'];
-			$_SESSION['usu_tipus'] = $usuari['usu_tipus'];
-			if ($_SESSION['usu_id'] == $usuari['usu_id'] AND $usuari['usu_tipus'] == 'admin') {
-				echo "Hola, " . $_SESSION['usu_nom'] . " ets un/a " . $_SESSION['usu_tipus'] . ".<br>";
-			}
-			echo "<a href='gestioPerfils.php'>Gestionar Perfils</a>";
-		}
-	} else {
-		header('location:../../index.php?err=1');
-	}                      
+// $sql = "SELECT * FROM tbl_usuari WHERE usu_id = " . $_SESSION['usu_id'] . "";
+// $resultat=mysqli_query($conexio, $sql);
+// 				// echo "foisndfuignfdiuog";
+// 	if (mysqli_num_rows($resultat) != 0 ) {
+// 		while ($usuari = mysqli_fetch_array($resultat)) {
+// 			$_SESSION['usu_id'] = $usuari['usu_id'];
+// 			$_SESSION['usu_tipus'] = $usuari['usu_tipus'];
+// 			if ($_SESSION['usu_id'] == $usuari['usu_id'] AND $usuari['usu_tipus'] == 'admin') {
+// 				echo "Hola, " . $_SESSION['usu_nom'] . " ets un/a " . $_SESSION['usu_tipus'] . ".<br>";
+// 			}
+// 			echo "<a href='gestioPerfils.php'>Gestionar Perfils</a>";
+// 		}
+// 	} else {
+// 		header('location:../../index.php?err=1');
+// 	}                      
 
  ?>
