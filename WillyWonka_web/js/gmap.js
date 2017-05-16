6687081c977ba154c9fc8afcc39b73a811c945de
10000585 <10000585.joan23@fje.edu>
2017-05-16 14:36:34 +0200

    js

---
+++ WillyWonka_web/js/gmap.js
@@ -0,0 +1,49 @@
+(function ($) {
+			$( ".gmap-btn" ).click(function(){
+				$( "#map-btn1" ).toggleClass( "btn-show", "btn-hide", 1000 );
+				$( "#map-btn1" ).toggleClass( "btn-hide", "btn-show", 1000 );
+				$( "#map-btn2" ).toggleClass( "btn-hide", "btn-show", 1000 );
+				$( "#map-btn2" ).toggleClass( "btn-show", "btn-hide", 1000 );
+				$( "#map-btn2" ).toggleClass( "close-map-button", "open-map-button", 1000 );
+				$( "#map-btn2" ).toggleClass( "open-map-button", "close-map-button", 1000 );
+				$( "#map" ).toggleClass( "close-map", "open-map", 1000 );
+				$( "#map" ).toggleClass( "open-map", "close-map", 1000 );
+				return false;
+			});
+
+			var folsom = new google.maps.LatLng(42.698162, 0.794008);
+			var markerposition = new google.maps.LatLng(42.698162, 0.794008);
+
+			var marker;
+			var map;
+
+			function initialize() {
+			  var mapOptions = {
+				zoom: 15,
+				center: folsom,  
+			  };
+
+			  map = new google.maps.Map(document.getElementById('google-map'),
+					  mapOptions);
+
+			  marker = new google.maps.Marker({
+				map:map,
+				draggable:false,
+				animation: google.maps.Animation.DROP,
+				position: markerposition
+			  });
+			  google.maps.event.addListener(marker, 'click', toggleBounce);
+			}
+
+			function toggleBounce() {
+
+			  if (marker.getAnimation() != null) {
+				marker.setAnimation(null);
+			  } else {
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+			  }
+			}
+
+			google.maps.event.addDomListener(window, 'load', initialize);
+	
+})(jQuery);
