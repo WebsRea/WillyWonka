62d60bd00b754f6607b427c11648d0c9aa67bfbe
rogerfus <10000712.joan23@fje.edu>
2017-05-17 19:11:49 +0200

    envia pass al canviar la contrasenya

--- WillyWonka_web/php/procs/canviarPass.proc.php
+++ WillyWonka_web/php/procs/canviarPass.proc.php
@@ -12,6 +12,7 @@ $results=mysqli_query($conexio,$sql);
 	if (mysqli_num_rows($results) != 0){
 		while ($usu = mysqli_fetch_array($results)) {
 			$pass_antiga_sql = $usu['usu_pass'];
+			$usu_mail = $usu['usu_mail'];
 		}
 	}
 
@@ -39,6 +40,8 @@ if (isset($_SESSION['errors'])) {
 	$pass_nova1 = hash('sha512', $pass_nova1);
 	$sqlUptdate = "UPDATE `tbl_usuari` SET `usu_pass` = '$pass_nova1' WHERE `tbl_usuari`.`usu_id` = $usu_id;";
 	$results=mysqli_query($conexio,$sqlUptdate);
+	$message = "S'ha restablert la contrasenya per al correu: $usu_mail <br> Ara es: $pass_nova2":
+	mail($usu_mail, 'Nova contrasenya', $message);
 	header('Location:../paginaPrincipal.php');
 
 }
