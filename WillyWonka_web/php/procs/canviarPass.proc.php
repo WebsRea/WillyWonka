274072ceb8d3699265a2df1d1821f5e88dde0fae
rogerfus <10000712.joan23@fje.edu>
2017-05-17 18:47:23 +0200

    Canviar Pass

---
+++ WillyWonka_web/php/procs/canviarPass.proc.php
@@ -0,0 +1,45 @@
+<?php session_start();
+include('../conexio.php');
+extract($_REQUEST);
+if (!isset($_SESSION['usu_id'])) {
+	header('Location:../index.php');
+}else{
+	$usu_id = $_SESSION['usu_id'];
+}
+
+$sql = "SELECT * FROM tbl_usuari WHERE usu_id = $usu_id";
+$results=mysqli_query($conexio,$sql);
+	if (mysqli_num_rows($results) != 0){
+		while ($usu = mysqli_fetch_array($results)) {
+			$pass_antiga_sql = $usu['usu_pass'];
+		}
+	}
+
+$pass_antiga = hash('sha512', $pass_antiga);
+if ($pass_antiga != $pass_antiga_sql) {
+	if (isset($_SESSION['errors'])) {
+		$_SESSION['errors'] .= "<br> Contrasenya antiga erronea.";
+	}else{
+		$_SESSION['errors'] = "Contrasenya antiga erronea.";
+	}
+}
+
+if ($pass_nova1 != $pass_nova2) {
+	if (isset($_SESSION['errors'])) {
+		$_SESSION['errors'] .= "<br> Las contraseñas no coinciden";
+	}else{
+		$_SESSION['errors'] = "Las contraseñas no coinciden";
+	}
+}
+
+if (isset($_SESSION['errors'])) {
+	header('Location:../canviarPass.php');
+}else{
+
+	$pass_nova1 = hash('sha512', $pass_nova1);
+	$sqlUptdate = "UPDATE `tbl_usuari` SET `usu_pass` = '$pass_nova1' WHERE `tbl_usuari`.`usu_id` = $usu_id;";
+	$results=mysqli_query($conexio,$sqlUptdate);
+	header('Location:../paginaPrincipal.php');
+
+}
+ ?>
