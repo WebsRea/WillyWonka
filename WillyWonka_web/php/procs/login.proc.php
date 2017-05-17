a26c18753db7df15397b9119c37f2bb22ae8425a
rogerfus <10000712.joan23@fje.edu>
2017-05-17 20:23:21 +0200

    envia mail

--- WillyWonka_web/php/procs/login.proc.php
+++ WillyWonka_web/php/procs/login.proc.php
@@ -15,10 +15,13 @@ $resultat=mysqli_query($conexio, $sql);
 			$_SESSION['usu_id'] = $usuari['usu_id'];
 			$_SESSION['usu_tipus'] = $usuari['usu_tipus'];
 			$_SESSION['usu_nom'] = $usuari['usu_nom'] . " " . $usuari['usu_cognoms'];
+			if ($usuari['usu_pass'] = "3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2") {
+				$_SESSION['pass123'] = "<a href='../canvarPass.php'>Et recomenem canviar la contrasenya</a>";
+			}
 			header('Location:../paginaPrincipal.php');
 		}
 	} else {
-		//header('location:../../index.php?err=1');
+		header('location:../../index.php?err=1');
 		echo "$sql";
 	}                      
 ?>
