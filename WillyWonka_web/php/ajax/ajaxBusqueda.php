1ec7d694b762f08c535c061841a64e3c3e52afc4
10000585 <10000585.joan23@fje.edu>
2017-05-18 16:21:29 +0200

    Front-end
    Empiezo con el tema de la clase, me parece que hay que revisar el código

--- WillyWonka_web/php/ajax/ajaxBusqueda.php
+++ WillyWonka_web/php/ajax/ajaxBusqueda.php
@@ -55,13 +55,10 @@ switch ($decisioBusqueda) {
 
 		break;
 	case 'eliminaNen':
-
-		if ($busqueda != "") {
-		
 			$sql = "SELECT * FROM tbl_nen WHERE nen_nom LIKE '%$busqueda%' OR nen_cognoms LIKE '%$busqueda%'";
 			$resultat = mysqli_query($conexio, $sql);
 			if (mysqli_num_rows($resultat) != 0 ) {
-				echo "<table><tr><h1><td>Nom</td><td>Cognom</td><td>Acció</td><h1></tr>";
+				echo "<table class='table table-hover'><tr class='datos_tabla'><th>Nom</th><th>Cognom</th><th>Acció</th></tr>";
 				while ($nen = mysqli_fetch_array($resultat)) {
 					$id = $nen['nen_id'];
 					echo "<tr><td>" . $nen['nen_nom'] . "</td><td>" . $nen['nen_cognoms'] . "</td><td><a href='#' onclick='eliminarNen($id)'>Eliminar</a></td></tr>";
@@ -70,59 +67,27 @@ switch ($decisioBusqueda) {
 			}else{echo "No hi han dades :'(";}
 			// echo "$sql";
 			
-		}else{
-			$sql = "SELECT * FROM tbl_nen WHERE nen_nom LIKE '%$busqueda%' OR nen_cognoms LIKE '%$busqueda%'";
-			$resultat = mysqli_query($conexio, $sql);
-			if (mysqli_num_rows($resultat) != 0 ) {
-				echo "<table><tr><h1><td>Nom</td><td>Cognom</td><td>Acció</td><h1></tr>";
-				while ($nen = mysqli_fetch_array($resultat)) {
-					$id = $nen['nen_id'];
-					echo "<tr><td>" . $nen['nen_nom'] . "</td><td>" . $nen['nen_cognoms'] . "</td><td><a href='#' onclick='eliminarNen($id)'>Eliminar</a></td></tr>";
-				}
-				echo "</table>";
-			}else{echo "No hi han dades :'(";}
-			// echo "$sql";
-			
-		}
-
 		break;
 	case 'eliminaTutor':
-		if ($busqueda != "") {
+			$sql = "SELECT * FROM tbl_usuari WHERE (usu_nom LIKE '%$busqueda%' OR usu_cognoms LIKE '%$busqueda%') AND usu_tipus = 'tutor'";
+			$resultat = mysqli_query($conexio, $sql);
+			if (mysqli_num_rows($resultat) != 0 ) {
+				echo "<table class='table table-hover'><tr class='datos_tabla'><th>Nom</th><th>Cognom</th><th>Acció</th></tr>";
+				while ($usu = mysqli_fetch_array($resultat)) {
+					$id = $usu['usu_id'];
+					echo "<tr><td>" . $usu['usu_nom'] . "</td><td>" . $usu['usu_cognoms'] . "</td><td><a href='#' onclick='eliminarUsu($id)'>Eliminar</a></td></tr>";
+				}
+				echo "</table>";
+			}else{echo "No hi han dades :'(";}
+			// echo "$sql";
+			
 		
-			$sql = "SELECT * FROM tbl_usuari WHERE (usu_nom LIKE '%$busqueda%' OR usu_cognoms LIKE '%$busqueda%') AND usu_tipus = 'tutor'";
-			$resultat = mysqli_query($conexio, $sql);
-			if (mysqli_num_rows($resultat) != 0 ) {
-				echo "<table><tr><h1><td>Nom</td><td>Cognom</td><td>Acció</td><h1></tr>";
-				while ($usu = mysqli_fetch_array($resultat)) {
-					$id = $usu['usu_id'];
-					echo "<tr><td>" . $usu['usu_nom'] . "</td><td>" . $usu['usu_cognoms'] . "</td><td><a href='#' onclick='eliminarUsu($id)'>Eliminar</a></td></tr>";
-				}
-				echo "</table>";
-			}else{echo "No hi han dades :'(";}
-			// echo "$sql";
-			
-		}else{
-			$sql = "SELECT * FROM tbl_usuari WHERE (usu_nom LIKE '%$busqueda%' OR usu_cognoms LIKE '%$busqueda%') AND usu_tipus = 'tutor'";
-			$resultat = mysqli_query($conexio, $sql);
-			if (mysqli_num_rows($resultat) != 0 ) {
-				echo "<table><tr><h1><td>Nom</td><td>Cognom</td><td>Acció</td><h1></tr>";
-				while ($usu = mysqli_fetch_array($resultat)) {
-					$id = $usu['usu_id'];
-					echo "<tr><td>" . $usu['usu_nom'] . "</td><td>" . $usu['usu_cognoms'] . "</td><td><a href='#' onclick='eliminarUsu($id)'>Eliminar</a></td></tr>";
-				}
-				echo "</table>";
-			}else{echo "No hi han dades :'(";}
-			// echo "$sql";
-			
-		}
 		break;
 	case 'eliminaMestre':
-		if ($busqueda != "") {
-		
 			$sql = "SELECT * FROM tbl_usuari WHERE (usu_nom LIKE '%$busqueda%' OR usu_cognoms LIKE '%$busqueda%') AND usu_tipus = 'mestre'";
 			$resultat = mysqli_query($conexio, $sql);
 			if (mysqli_num_rows($resultat) != 0 ) {
-				echo "<table><tr><h1><td>Nom</td><td>Cognom</td><td>Acció</td><h1></tr>";
+				echo "<table class='table table-hover'><tr class='datos_tabla'><th>Nom</th><th>Cognom</th><th>Acció</th></tr>";
 				while ($usu = mysqli_fetch_array($resultat)) {
 					$id = $usu['usu_id'];
 					echo "<tr><td>" . $usu['usu_nom'] . "</td><td>" . $usu['usu_cognoms'] . "</td><td><a href='#' onclick='eliminarUsu($id)'>Eliminar</a></td></tr>";
@@ -131,20 +96,6 @@ switch ($decisioBusqueda) {
 			}else{echo "No hi han dades :'(";}
 			// echo "$sql";
 			
-		}else{
-			$sql = "SELECT * FROM tbl_usuari WHERE (usu_nom LIKE '%$busqueda%' OR usu_cognoms LIKE '%$busqueda%') AND usu_tipus = 'mestre'";
-			$resultat = mysqli_query($conexio, $sql);
-			if (mysqli_num_rows($resultat) != 0 ) {
-				echo "<table><tr><h1><td>Nom</td><td>Cognom</td><td>Acció</td><h1></tr>";
-				while ($usu = mysqli_fetch_array($resultat)) {
-					$id = $usu['usu_id'];
-					echo "<tr><td>" . $usu['usu_nom'] . "</td><td>" . $usu['usu_cognoms'] . "</td><td><a href='#' onclick='eliminarUsu($id)'>Eliminar</a></td></tr>";
-				}
-				echo "</table>";
-			}else{echo "No hi han dades :'(";}
-			// echo "$sql";
-			
-		}
 		break;
 	
 	default:
--- WillyWonka_web/php/gestioClasses.php
+++ WillyWonka_web/php/gestioClasses.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<?php
+		session_start();
+		include "includes_admin/head.php";
+	?>
 	<title>Gestió de classes</title>
 	<script src="http://code.jquery.com/jquery.js"></script>
 	<script type="text/javascript">
@@ -61,10 +65,54 @@
 	</script>
 
 </head>
-<body>
-<?php session_start();
 
-include('conexio.php');
+<body id="inicio" class="admin">
+
+    <!-- Empezamos con el nav -->
+    <nav class="navbar navbar-fixed-top navegador"  role="navigation">
+        <div class="container">
+            <!-- Creamos el header del menu -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" >
+                    <span class="sr-only">Alternar menu</span>
+                    <img src="../img/001-chocolate.png"></img>               
+                </button>
+                <a href="index_admin.php"><img class="img-responsive" src="../img/LogoWW_SD.png" alt=""></a>
+            </div>
+            <!-- Links del menu -->
+            <div class="collapse navbar-collapse align-right" id="navbar1">
+                 <div class="nav navbar-nav botonesnav">
+                    <a href="gestioPerfils.php" class="btn btn-willy btn-lg">
+                    <img src="../img/001-chocolate.png"></img>Perfils </a>
+                    <a href="gestioClasses.php" class="btn btn-willy btn-lg">
+                    <img src="../img/001-chocolate.png"></img>Classes</a>
+                    <a href="#" class="btn btn-willy btn-lg">
+                    <img src="../img/001-chocolate.png"></img>Esdeveniments</a>
+                    <a href="#" class="btn btn-willy btn-lg" >
+                    <img src="../img/001-chocolate.png"></img> Documents</a>
+                    <a href="#" class="btn btn-willy btn-lg" >
+                    <img src="../img/001-chocolate.png"></img> Contacte</a>
+                </div>
+            </div>
+            <div class="container">
+                <h4 class="h4inicio">
+                <div class="row">
+                <?php 
+                echo "Perfecte! modifiquem alguns perfils " . $_SESSION['usu_nom'] . " o prefereixes  <a href='../index.php'><i class='fa fa-power-off' aria-hidden='true'> Sortir</i></a> ?<br>";
+                ?>
+                </div>
+                </h4>
+            </div>
+
+        </div>
+    </nav>
+    <!-- /.nav -->
+   
+    <div class="jumbotron frase">
+
+    	<div class="container padmin">
+    	<?php 
+    	include('conexio.php');
 
 $sql = "SELECT * FROM tbl_classe";
 
@@ -85,5 +133,14 @@ $resultat=mysqli_query($conexio, $sql);
 ?><br><br>
 <a href="#" onclick="veureAfegirClasse()">Afegir una classe</a><br><br>
 <div id="afegirClasse"></div>
+    		
+		</div> 
+	</div>
+    
 </body>
+<footer class="footer">
+  <?php
+  include "includes_admin/footer.php";
+  ?>
+</footer>
 </html>
