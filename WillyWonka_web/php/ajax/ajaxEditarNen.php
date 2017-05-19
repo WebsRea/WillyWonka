d958ffa2f2443a75d1d1e612695756de9754572d
10000585 <10000585.joan23@fje.edu>
2017-05-19 16:14:45 +0200

    Editar nene

--- WillyWonka_web/php/ajax/ajaxEditarNen.php
+++ WillyWonka_web/php/ajax/ajaxEditarNen.php
@@ -15,23 +15,28 @@ $sql = "SELECT * FROM tbl_nen WHERE nen_id = $id";
 					$trastorns = $nen['nen_trastorns'];
 					$malalties = $nen['nen_malaltia'];
 					echo "
-						<form action='procs/editarNen.proc.php'><br><br>
-						nom:
-						<input type='text' name='nom' value='$nom'><br>
-						cognoms:
-						<input type='text' name='cognoms' value='$cognoms'><br>
-						Data naixament
-						<input type='date' name='data' value='$data'><br>
-						alergies:
-						<input type='text' name='alergies' value='$alergies'><br>
-						trastorns:
-						<input type='text' name='trastorns' value='$trastorns'><br>
-						malalties:
-						<input type='text' name='malalties' value='$malalties'><br>
-						<input type='hidden' name='id' value='$id'><br>
-						<input type='submit' value='Modificar'>
+						<div class='jumbotron frase'><h2 class='h2fam'>Estàs a punt de canviar la fitxa de:</h2></div>
+						<form action='procs/editarNen.proc.php'>
+						<div class='col-md-4'></div>
+						<div class='col-md-4'>
+							<div class='form-group'>
+	                    	<label>Nom</label>
+							<input type='text' name='nom' value='$nom' class='form-control'>
+							<label>Cognoms</label>
+							<input type='text' name='cognoms' value='$cognoms' class='form-control'>
+							<label>Data naixament</label>
+							<input type='date' name='data' value='$data' class='form-control'>
+							<label>Alergies</label>
+							<input type='text' name='alergies' value='$alergies' class='form-control'>
+							<label>Trastorns</label>
+							<input type='text' name='trastorns' value='$trastorns' class='form-control'>
+							<label>Malalties</label>
+							<input type='text' name='malalties' value='$malalties' class='form-control'>
+							<input type='hidden' name='id' value='$id'><br> 
+							<input type='submit' value='Modificar' class='btn btn-willy text-center'>
+							</div>
+						</div> 
 					";
-
 
 					// $sqlUpdate = "UPDATE `tbl_nen` SET `nen_nom` = '$nom', `nen_cognoms` = '$cognoms', `nen_data_naixement` = '$data', `nen_alergies` = '$alergies', `nen_trastorns` = '$trastorns', `nen_malaltia` = '$malalties' WHERE `tbl_nen`.`nen_id` = $id;"
 				}
