4befb3d4cbf63f8768bfb5ec606373bb6ed96255
10000585 <10000585.joan23@fje.edu>
2017-05-20 19:13:03 +0200

    Front end

--- WillyWonka_web/php/ajax/ajaxEditarMestre.php
+++ WillyWonka_web/php/ajax/ajaxEditarMestre.php
@@ -13,16 +13,20 @@ $sql = "SELECT * FROM tbl_usuari WHERE usu_id = $id AND usu_tipus = 'mestre'";
 					$mail = $usu['usu_mail'];
 					$pass = $usu['usu_pass'];
 					echo "
+						<div class='jumbotron frase'><h2 class='h2fam'>Recorda:<small class='peque'> si cliques per modificar la contrasenya no quedaran guardats la resta de canvis</small></h2></div>
 						<form action='procs/editarMestre.proc.php'><br><br>
-						nom:
-						<input type='text' name='nom' value='$nom'><br>
-						cognoms:
-						<input type='text' name='cognoms' value='$cognoms'><br>
-						Mail:
-						<input type='text' name='mail' value='$mail'><br>
+						<div class='col-md-4'></div>
+						<div class='col-md-4'>
+							<div class='form-group'>
+		                    	<label>Nom</label>
+						<input type='text' name='nom' value='$nom' class='form-control'>
+						<label>Cognoms</label>
+						<input type='text' name='cognoms' value='$cognoms' class='form-control'>
+						<label>Mail</label>
+						<input type='text' name='mail' value='$mail' class='form-control'>
 						<a href='procs/resetPass.proc.php?id=$id'>Clica per resetejar la contrasenya (serà 123 fins que la canvíin)</a><br>
 						<input type='hidden' name='id' value='$id'><br>
-						<input type='submit' value='Modificar'>
+						<input type='submit' value='Modificar' class='btn btn-willy text-center'>
 					";
 				}
 				echo "</table>";
