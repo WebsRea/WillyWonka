da0a9319abc6a0691e4f2f93db9b79e75d07436d
rogerfus <10000712.joan23@fje.edu>
2017-05-22 17:24:12 +0200

    fent mail pares stock

--- WillyWonka_web/php/ajax/ajaxEditarStonen.php
+++ WillyWonka_web/php/ajax/ajaxEditarStonen.php
@@ -5,6 +5,17 @@ extract($_REQUEST);
 $sql = "UPDATE `tbl_stock_nen` SET `stonen_quantitat` = '$quantitat' WHERE `tbl_stock_nen`.`stonen_id` = $stonen_id;";
 	include('../conexio.php');
 	$resultat = mysqli_query($conexio, $sql);
+
+	$sqlMail = "SELECT * FROM tbl_stock_nen WHERE stonen_id = $stonen_id";
+	$resultatMail = mysqli_query($conexio, $sqlMail);
+	if (mysqli_num_rows($resultatMail) != 0 ) {
+		while ($stoMail = mysqli_fetch_array($resultatMail)) {
+			
+		}
+	}
+
+
+
 	echo "O SALES O MATO A ALGUIEN";
 	
 
