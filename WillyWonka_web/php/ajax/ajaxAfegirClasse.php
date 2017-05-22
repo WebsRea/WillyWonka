ceb92d15f14671a50ea2b89ee442ba1a5ad0c946
10000585 <10000585.joan23@fje.edu>
2017-05-22 20:10:21 +0200

    Corrección

--- WillyWonka_web/php/ajax/ajaxAfegirClasse.php
+++ WillyWonka_web/php/ajax/ajaxAfegirClasse.php
@@ -8,20 +8,21 @@ echo '
 		</div>
 		<div class="row">
 			<div class="form-group">
-			<label class="psom">Selecciona mestre: <small>Utilitza el desplegable per trobar de forma ràpida un mestre</label>
-			<form action="procs/afegirClasse.proc.php"  class="psom">
-				<div id="resultadosBusqueda">
-				<select name="mestreId"></select>
-				</div>
-				</div>
-			<div class="form-group">
-			<label class="psom">Nom de la classe: </label> <input type="text" name="cla_nom"><br>
-			
-			<label class="psom">Franja: </label> <input type="number" name="cla_curs" min="0" max="3"><br>
+				<label class="psom">Selecciona mestre: <small>Utilitza el desplegable per trobar de forma ràpida un mestre</label>
+				<form action="procs/afegirClasse.proc.php"  class="psom">
+						<div id="resultadosBusqueda">
+							<select name="mestreId"></select>
+						</div>
+		
+							<label class="psom">Nom de la classe: </label> <input type="text" name="cla_nom"><br>
+							
+							<label class="psom">Franja: </label> <input type="number" name="cla_curs" min="0" max="3"><br>
 
-			<label class="psom">Foto per la classe:</label> <input type="text" name="cla_foto" disabled placeholder="deshabilitat"><br>
-			<input type="submit" value="enviar" class="btn btn-willy text-center btn-lg">
-		</div>	
+							<label class="psom">Foto per la classe:</label> <input type="text" name="cla_foto" disabled placeholder="deshabilitat"><br>
+							<input type="submit" value="enviar" class="btn btn-willy text-center btn-lg">
+					
+			</div>	
+
 ';
  ?>
 	
