f07339ee039ec1fca122b47e4548788d09850db4
10000585 <10000585.joan23@fje.edu>
2017-05-25 09:30:21 +0200

    Front

--- WillyWonka_web/php/mostrarEsdeveniment.php
+++ WillyWonka_web/php/mostrarEsdeveniment.php
@@ -34,7 +34,7 @@ include 'conexio.php';
       $results=mysqli_query($conexio,$sql);
       if ($row = mysqli_fetch_array($results)){ 
          echo "<table class='table-hover'> \n"; 
-         echo "<tr class='datos_tabla padmin'><td>Titol</td><td>&nbsp;&nbsp;Descripció</td><td>&nbsp;&nbsp;Inici</td><td>&nbsp;&nbsp;Fi</td></tr> \n"; 
+         echo "<tr class='datos_tabla padmin'><td>Títol</td><td>&nbsp;&nbsp;Descripció</td><td>&nbsp;&nbsp;Inici</td><td>&nbsp;&nbsp;Fi</td></tr> \n"; 
          do { 
             echo "<tr class='info'><td>".$row["esd_titol"]."</td><td>&nbsp;&nbsp;".$row["esd_text"]."</td><td>&nbsp;&nbsp;".$row["esd_data_ini"]."</td><td>&nbsp;&nbsp;".$row["esd_data_fin"]."</td><td><a href='procs/eliminarEsdeveniment.proc.php?esd_id=".$row['esd_id']."''>eliminar</a></td></tr> \n"; 
          } while ($row = mysqli_fetch_array($results)); 
